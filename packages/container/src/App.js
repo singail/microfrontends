42b762d9b33241989536c8c3f4c15483d55a5ad1
Rimgaile <rimgaile.sinkeviciene@devbridge.com>
2023-08-01 10:32:29 +0300

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
+
+export default () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <h1>App</h1>
+      <hr />
+      <MarketingApp />
+    </BrowserRouter>
+  );
+};
