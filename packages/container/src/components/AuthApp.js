2a822e743f4c8a02c67276ef07ec9df49fc2b76a
Rimgaile <rimgaile.sinkeviciene@devbridge.com>
2023-08-03 12:35:15 +0300

    auth app

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,4 +1,4 @@
-import { mount } from "marketing/MarketingApp";
+import { mount } from "auth/AuthApp";
 import React, { useRef, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
@@ -8,6 +8,7 @@ export default () => {
 
   useEffect(() => {
     const { onParentNavigate } = mount(ref.current, {
+      initialPath: history.location.pathname,
       onNavigate: ({ pathname: nextPathName }) => {
         const { pathname } = history.location;
         if (pathname !== nextPathName) {
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,16 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createMemoryHistory, createBrowserHistory } from "history";
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathName }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathName) {
+        history.push(nextPathName);
+      }
+    },
+  };
 };
 
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
